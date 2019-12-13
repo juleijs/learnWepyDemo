2e8ce946688c3725c5ba569d06b9f5f18d125d8a
julei <jujuleilei@gmail.com>
2019-12-13 19:48:33 +0800

    提交代码2019_12_13_02

---
+++ src/common/api.js
@@ -0,0 +1,9 @@
+import m_contacts from './../mocks/contact'
+import m_history from './../mocks/history'
+import m_reply from './../mocks/reply'
+import global from './global'
+import wepy from 'wepy'
+
+export default {
+
+}
